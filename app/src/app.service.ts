da4444558019be3adbd3a7df8a005da50a61c49c
majurr <marionascimentojunior@hotmail.com>
2023-05-22 13:32:10 -0300

    correct pgadmin service and add redis-commander service. add env.sample

--- app/src/app.service.ts
+++ app/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'App NestJS - Nginx - PostgreSQQL- Redis e pgAdmin 4';
   }
 }
